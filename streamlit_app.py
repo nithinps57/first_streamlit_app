24060f0b8138842b1c53f5d5ac36a54cd2306c94
nithinps57 <129928056+nithinps57@users.noreply.github.com>
2023-04-05 11:32:43 +0530

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,9 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast menu')
 
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale ,Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 Kale ,Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+ streamlit.text('🥑🍞Avocado tost')
 
 
