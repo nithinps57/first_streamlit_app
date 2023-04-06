6529e861014ab78d5a06039e143030ed5d6165cd
nithinps57 <129928056+nithinps57@users.noreply.github.com>
2023-04-06 19:41:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ def get_fruitvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
    # streamlit.text(fruityvice_response.json())--to print raw json
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   returns fruityvice_normalized
+   return fruityvice_normalized
 
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 try:
