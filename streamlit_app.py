09a6f6a0159e8b67db91dc379d9e9b395a5efd62
nithinps57 <129928056+nithinps57@users.noreply.github.com>
2023-04-06 19:23:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ try:
       # streamlit.text(fruityvice_response.json())--to print raw json
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
-except URLError as e;
+except URLError as e:
   Sreamlit.error()
 
 
